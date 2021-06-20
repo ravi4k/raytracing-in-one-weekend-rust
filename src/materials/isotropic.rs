342cb32a3b35a496d6dd76b7832c0600da4978bb
Ravi K <ravi4kumawat@gmail.com>
2021-06-20 21:05:38 +0530

    Fix Constant Density Medium

--- src/materials/isotropic.rs
+++ src/materials/isotropic.rs
@@ -14,9 +14,9 @@ pub struct Isotropic {
 impl Material for Isotropic {
     fn scatter(&self, in_ray: Ray, hit_rec: &HitRecord) -> Option<ScatterRecord> {
         return Option::from(ScatterRecord {
-            specular_ray: Option::from(Ray { origin: hit_rec.intersection, direction: Vector3::random_unit_vector(), time: in_ray.time })
+            specular_ray: Option::from(Ray { origin: hit_rec.intersection, direction: Vector3::random_unit_vector(), time: in_ray.time }),
             attenuation: self.albedo.color(hit_rec.u, hit_rec.v, hit_rec.intersection),
-            pdf_ptr: None
+            pdf_ptr: None,
         });
     }
 }
